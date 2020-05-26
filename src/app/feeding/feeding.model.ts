18b75d8c500adc515902edb03e1f016c395be816
dalanienatsch <dienatsch93@gmail.com>
2020-05-26 15:36:27 -0600

    Updated feeding model

--- src/app/feeding/feeding.model.ts
+++ src/app/feeding/feeding.model.ts
@@ -1,14 +1,14 @@
 export class Feeding {
     public feedingId: string;
-    public date: string;
-    public time: string;
+    public feedingDate: string;
+    public feedingTime: string;
     public oz: number;
     public timeSinceLastFeed: string;
 
     constructor(feedingId, date, time, oz, timeSinceLastFeed) {
         this.feedingId = feedingId
-        this.date = date;
-        this.time = time;
+        this.feedingDate = date;
+        this.feedingTime = time;
         this.oz = oz;
         this.timeSinceLastFeed = timeSinceLastFeed;
     }
