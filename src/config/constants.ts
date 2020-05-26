0ae136415996f6807ecf00b18ea144efa5445277
dalanienatsch <dienatsch93@gmail.com>
2020-05-26 15:36:59 -0600

    Fixed API endpoint config

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Constants {
-    public readonly API_ENDPOINT: string = 'https://localhost:44305/api/values';
+    public readonly API_ENDPOINT: string = 'https://localhost:44305/api';
     public readonly API_MOCK_ENDPOINT: string = 'mock-domain/api';
 }
